57007b274884b7a8f068a67023b92b1b8e190a26
Sam Rust <xsrust@gmail.com>
2020-01-04 01:13:31 +0000

    pastebin init

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,51 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, world!"
+extern crate rand;
+
+mod paste_id;
+
+use paste_id::PasteID;
+
+use std::io;
+use std::path::Path;
+use std::fs::File;
+
+use rocket::Data;
+
+#[get("/")]
+fn index() -> &'static str {
+    "
+    USAGE
+
+      POST /
+
+          accepts raw data in the body of the request and responds with a URL of
+          a page containing the body's content
+
+      GET /<id>
+
+          retrieves the content for the paste with id `<id>`
+    "
+}
+
+#[post("/", data= "<paste>")]
+fn upload(paste: Data) -> io::Result<String> {
+    let id = PasteID::new(7);
+    let filename = format!("upload/{id}", id = id );
+    let url = format!("{host}/{id}\n", host = "http://localhost:8000", id = id);
+
+    // write the paste out to the file and return the URL.
+    paste.stream_to_file(Path::new(&filename))?;
+    Ok(url)
+}
+
+#[get("/<id>")]
+fn retrieve(id: PasteID) -> Option<File> {
+    let filename = format!("upload/{id}", id = id);
+    File::open(&filename).ok()
 }
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![world]).launch();
+    rocket::ignite().mount("/", routes![index, upload, retrieve]).launch();
 }
